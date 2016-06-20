851893ba628e412dd0f957a8f1a34533c68e4179
leanne <811873539@qq.com>
2016-06-21 00:00:13 +0800

    creat environment

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+var path = {
+    baseDir:'./'
+}
+
+// Static server
+gulp.task('browser-sync', function() {
+    var files = [
+    './test/**/*'
+    ];
+    browserSync.init(files,{
+        server: {
+            baseDir: './'
+        }
+    });
+}); 
+
+// Domain server
+//gulp.task('brows er-sync', function() {
+//    browserSync.init({
+//        proxy: "yourlocal.dev"
+//    });  
+//});
+
+gulp.task('default',['browser-sync'],function(files){
+    gulp.watch(path.baseDir).on('change',function(){
+        console.log(files);
+        browserSync.reload; 
+    });
+}); 
